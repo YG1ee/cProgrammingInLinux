b40e9be26d9ef2ac2bc5faefc337096571a6c2c1
YG1ee <YG1ee@users.noreply.github.com>
2017-04-13 12:43:31 +0900

    원래 의미의 mycat 구현
    입력 파일이 20개가 넘으면 프로그램을 종료하도록 했지만 20개가 넘으면 넘는 파일을 무시하고 규격에 맞는 파일은 출력하도록 변경.

--- cat Implement/mycatImple.c
+++ cat Implement/mycatImple.c
@@ -1,84 +1,103 @@
+// 줄번호 옵션만 구현
+
 #include <stdio.h>
+#define MAX_CNT 20
 
 int main(int ac, char *av[]) {
     FILE *fp;
     int ch, i;
-    // int lineNum = 2; 줄번호 출력용 변수
+    int lineNum = 1; // 줄번호
+    int flag = 0; // lineNum을 출력할지 안 할지 정하는 플래그
+    int ismax = ac > MAX_CNT;
 
-    if(av[1][0] == '-') {                               // 옵션을 주는 경우.
-        if(ac > 22) {
-            printf("파일은 최대 20개까지만 출력할 수 있습니다.\n");
-            return -2;
-        }
-    /*
-     *  if(av[1][1] == 'n') {                           //  줄번호 출력 옵션.
-     *      for(i = 2; i < ac; i++) {
-     *          if( (fp = fopen(av[i], "r")) == NULL) {
-     *              printf("Can't open %s\n", av[i]);
-     *              fclose(fp);
-     *          }
-     *          else {
-     *              printf("1  ");
-     *              while( (ch = fgetc(fp)) != EOF) {   // fp 파일로부터 한 문자 읽기
-     *                  fputc(ch, stdout);              // 읽은 문자 표준 출력으로 보내기
-     *                  if(ch == 10) {                  // 10은 개행 문자의 아스키 값
-     *                      printf("%d  ", lineNum);
-     *                      lineNum += 1;
-     *                  }
-     *              }
-     *          }   
-     *          printf("\n");
-     *      }
-     *  }
-     */
-	    if(av[1][1] == 'c') {                      // 소문자로 바꿔서 출력
-            for(i = 2; i < ac; i++) {
-                if( (fp = fopen(av[i], "r")) == NULL )
-                        printf("Can't open %s\n", av[i]);
-                else {
-                    while( (ch = fgetc(fp)) != EOF) {   // fp 파일로부터 한 문자 읽기
-                        if( (ch >= 65) & (ch <= 90) )
-                            ch += 32;
-                        fputc(ch, stdout);              // 읽은 문자 표준 출력으로 보내기
+    if( (ac>=2)&&(av[1][0] == '-') ) {                      // 옵션을 주는 경우.
+        if(av[1][1] == 'n') {                  //  줄번호 출력 옵션.
+            if(ac == 2) {                            //  파일명을 입력하지 않은 경우
+                while( (ch = fgetc(stdin)) != EOF) { // stdin에서 문자 읽어오기
+                    if(lineNum == 1) {
+                            printf("%6d  ", lineNum);
+                            lineNum += 1;
                     }
-                    fclose(fp);
-        	    }
+                            
+                    else if(flag) {
+                        printf("%6d  ", lineNum);
+                        lineNum += 1;
+                        flag = 0;
+                    }
+
+                    if(ch == '\n')
+                        flag = 1;
+
+                    fputc(ch, stdout);
+                }
+                fclose(fp);
+                return 0;
+            }
+
+            else {
+                if(ac > MAX_CNT+2) {
+                    printf("파일은 최대 20개까지만 출력할 수 있습니다.\n");
+                    for(i = 0; i < ac-2-MAX_CNT; i++)
+                        printf("ignoring %s..\n", av[i+22]);
+                    printf("\n");
+                    ac = MAX_CNT+2;
+                }
+
+                for(i = 2; i < ac; i++) {
+                    if( (fp = fopen(av[i], "r")) == NULL)
+                        printf("Can't open %s\n", av[i]);
+                    else {
+                        while( (ch = fgetc(fp)) != EOF) {       // fp 파일로부터 문자 읽어오기
+                            if(lineNum == 1) {
+                                printf("%6d  ", lineNum);
+                                lineNum += 1;
+                            }
+        
+                            else if(flag) {
+                                printf("%6d  ", lineNum);
+                                lineNum += 1;
+                                flag = 0;
+                            }
+
+                            if(ch == 10)
+                                flag = 1;
+
+                            fputc(ch, stdout);
+                        }
+                        fclose(fp);
+                    }
+                }
             }
         }
-	    else if(av[1][1] == 'C') {                      // 대문자로 바꿔서 출력
-            for(i = 2; i < ac; i++) {
-                if( (fp = fopen(av[i], "r")) == NULL)
-                        printf("Can't open %s\n", av[i]);
-                else {
-                    while( (ch = fgetc(fp)) != EOF) {   // fp 파일로부터 한 문자 읽기
-                        if( (ch >= 95) & (ch <= 122) )
-                            ch -= 32;
-                        fputc(ch, stdout);              // 읽은 문자 표준 출력으로 보내기
-                    }
-                    fclose(fp);
-        	    }
+        else { // pass
+        printf("구현되지 않은 기능입니다.\n");
+        return -1;
+        }
+    }
+
+    else { // 옵션을 주지 않는 경우. 줄번호 미출력
+        if(ac == 1) { // 파일 미입력
+            while( (ch = fgetc(stdin)) != EOF) { // stdin에서 문자 읽어오기
+                fputc(ch, stdout);
             }
         }
         else {
-            // pass
-            printf("구현되지 않은 기능입니다.\n");
-            return -1;
-        }
-    }
-    else { // 옵션을 주지 않는 경우. 줄번호 미출력
-        if(ac > 21) {
+            if(ac > MAX_CNT+1) {
             printf("파일은 최대 20개까지만 출력할 수 있습니다.\n");
-            return -2;
-        }
-        for(i = 1; i < ac; i++) {
-            if( (fp = fopen(av[i], "r")) == NULL)
-                printf("Can't open %s\n", av[i]);
-            else {
-                while( (ch = fgetc(fp)) != EOF) {   // fp 파일로부터 한 문자 읽기
-                    fputc(ch, stdout);              // 읽은 문자 표준 출력으로 보내기
-                }
-                fclose(fp);
+                for(i = 0; i < ac-1-MAX_CNT; i++)
+                    printf("ignoring %s..\n", av[i+21]);
                 printf("\n");
+                ac = MAX_CNT+1;
+            }
+
+            for(i = 1; i < ac; i++) {
+                if( (fp = fopen(av[i], "r")) == NULL)
+                    printf("Can't open %s\n", av[i]);
+                else {
+                    while( (ch = fgetc(fp)) != EOF)   // fp 파일로부터 한 문자 읽기
+                        fputc(ch, stdout);              // 읽은 문자 표준 출력으로 보내기
+                    fclose(fp);
+                }
             }
         }
     }
