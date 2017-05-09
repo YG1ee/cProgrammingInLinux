1ef418ea3c60b20f82fe57e745d057522be9a59e
yg1ee <yg1ee@Yongui-MacBook-Pro.local>
2017-05-09 17:46:13 +0900

    fgrep 간단히 구현

---
+++ grep Implement/fgrep_.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int ac, char *av[]) {
+    FILE *fp;
+    char buf[200];
+    int i, j, state = 0;
+    int plen;
+
+    if(ac < 3) {
+        printf("Usage: %s pattern filename ..\n", av[0]);
+	return -1;
+    }
+    plen = strlen(av[1]);
+
+    for(i = 2; i < ac; i+=1) {
+        if( (fp = fopen(av[i], "r")) == NULL) {
+	    printf("Can't open %s\n", av[i]);
+	    return -2;
+	}
+
+	while(fgets(buf, sizeof(buf), fp) != NULL) {
+	    int blen = strlen(buf);
+	    for(j = 0; j < blen; j+=1) {
+	        if(buf[j] == av[1][state]) {
+		    state += 1;
+		    if(state == plen)
+		        printf("%s", buf);
+		}
+		else if(buf[j] == av[1][0])
+		    state = 1;
+		else
+		    state = 0;
+            }
+	}
+	fclose(fp);
+    }
+    return 0;
+}
+
