ccd0666fdef4e26e3880222f23565a17109f86a7
yg1ee <yg1ee@Yongui-MacBook-Pro.local>
2017-05-16 22:14:53 +0900

    indent 다듬기

--- grep Implement/fgrep_.c
+++ grep Implement/fgrep_.c
@@ -9,7 +9,7 @@ int main(int ac, char *av[]) {
 
     if(ac < 3) {
         printf("Usage: %s pattern filename ..\n", av[0]);
-	return -1;
+		return -1;
     }
     plen = strlen(av[1]);
 
@@ -17,24 +17,23 @@ int main(int ac, char *av[]) {
         if( (fp = fopen(av[i], "r")) == NULL) {
 	    printf("Can't open %s\n", av[i]);
 	    return -2;
-	}
-
-	while(fgets(buf, sizeof(buf), fp) != NULL) {
-	    int blen = strlen(buf);
-	    for(j = 0; j < blen; j+=1) {
-	        if(buf[j] == av[1][state]) {
-		    state += 1;
-		    if(state == plen)
-		        printf("%s", buf);
 		}
-		else if(buf[j] == av[1][0])
-		    state = 1;
-		else
-		    state = 0;
-            }
-	}
-	fclose(fp);
+
+		while(fgets(buf, sizeof(buf), fp) != NULL) {
+			int blen = strlen(buf);
+			for(j = 0; j < blen; j+=1) {
+				if(buf[j] == av[1][state]) {
+				state += 1;
+				if(state == plen)
+					printf("%s", buf);
+				}
+				else if(buf[j] == av[1][0])
+					state = 1;
+				else
+					state = 0;
+				}
+		}
+		fclose(fp);
     }
     return 0;
 }
-
